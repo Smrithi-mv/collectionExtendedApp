ca7aeaa3289482af1ca0db580377fe4fc8d2e2e6
Smrithi MV <smrithimv2003@gmail.com>
2025-07-08 18:29:29 +0530

    intr2

--- src/main/java/com/shanInfotech/collectionExtendedApp/App.java
+++ src/main/java/com/shanInfotech/collectionExtendedApp/App.java
@@ -5,6 +5,21 @@ package com.shanInfotech.collectionExtendedApp;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+InterviewScheduler scheduler = new InterviewScheduler();
+		
+		scheduler.scheduledInterview(new Interview(1, "Smrithi Mv", 93,"10:00 AM"));
+		scheduler.scheduledInterview(new Interview(2, "Sharanya Bk", 84,"10:30 AM"));
+		scheduler.scheduledInterview(new Interview(3, "adithya", 71,"11:00 AM"));
+		
+		scheduler.walkInCandidate(new Interview(4, "Deepak", 91,"NOW"));
+		
+		System.out.println("\nFull SCHEDULE");
+		scheduler.displaySchedule();
+		
+		System.out.println("\nWALK-IN");
+		scheduler.displayWalkIns();
+		
+		System.out.println("\nTOP PERFORMERS (Score >70)");
+		scheduler.displayTopScorers(70);
     }
 }
